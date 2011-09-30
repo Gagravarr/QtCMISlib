c0adb327bf04dfdfbb458d411400be94f969fa85
Nick Burch <nick@gagravarr.org>
2011-09-30 20:49:29 +0100

    Add demo project files

---
+++ demo/demo.h
@@ -0,0 +1,21 @@
+#ifndef CMIS_DEMO_H
+#define CMIS_DEMO_H
+
+#include <QCoreApplication>
+#include <QTextStream>
+#include <QTimer>
+
+#include <qtcmislib.h>
+#include <stdio.h>
+
+class Demo : public QObject
+{
+    Q_OBJECT
+
+public:
+    Demo();
+//public slots:
+//    void execute();
+};
+
+#endif // CMIS_DEMO_H
